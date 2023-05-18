68207856719bdef8485de8f6a33cbd02231e757d
Ibrahim Mustafa <ibrahim@nweave.com>
2023-05-18 06:08:00 +0300

    added edit action

--- components/TheTable.vue
+++ components/TheTable.vue
@@ -6,24 +6,74 @@
         <div class="list" v-if="isDropped">
           <draggable v-model="availableColumns" item-key="id">
             <template #item="{ element }">
-              <div>{{ element.name }}</div>
+              <div  draggable="true">{{ element.name }}</div>
             </template>
           </draggable>
         </div>
       </transition>
     </div>
-    <DataTable :value="data">
+    <DataTable
+    v-model:editingRows="editingRows"
+        :value="data"
+        editMode="row"
+        dataKey="id"
+        @row-edit-save="onRowEditSave"
+        tableClass="editable-cells-table"
+        tableStyle="min-width: 50rem">
       <Column
         v-for="column in availableColumns"
         :key="column.field"
         :field="column.field"
-        :header="column.header"
-      ></Column>
+        :header="column.header" filterField="column.name" filterMatchMode="contains" sortable
+      ><template  #editor="{ data, field }">
+          <InputText v-model="data[field]" />
+        </template></Column>
+      <Column :rowEditor="true" style="width: 10%; min-width: 8rem" bodyStyle="text-align:center"></Column>
+
     </DataTable>
   </div>
 </template>
 
 <script setup lang="ts">
+
+const props = defineProps({
+  view: {
+    required: false,
+    default: false
+  },
+  edit: {
+    required: false,
+    default: true
+  },
+  disable: {
+    required: false,
+    default: false
+  },
+  imperson: {
+    required: false,
+    default: false
+  },
+  handleView: {
+    type: Function,
+    required: false
+  },
+  handleEdit: {
+    type: Function,
+    required: false
+  },
+  handleDelete: {
+    type: Function,
+    required: false
+  },
+  handleDisable: {
+    type: Function,
+    required: false
+  },
+  handleImpersonate: {
+    type: Function,
+    required: false
+  }
+})
 const availableColumns = ref([
   { name: "name", field: "name", header: "Name" },
   { name: "age", field: "age", header: "Age" },
@@ -33,7 +83,13 @@ const availableColumns = ref([
 const isDropped = ref();
 const selectedColumns = ref(availableColumns.value);
 const isDragging = ref(false);
+const editingRows = ref([]);
 
+const onRowEditSave = (event) => {
+  let { newData, index } = event;
+
+  data.value[index] = newData;
+};
 const data = ref([
   { id: 1, name: "John", age: 25, city: "New York" },
   { id: 2, name: "Jane", age: 30, city: "London" },
@@ -43,6 +99,26 @@ const data = ref([
 const dropIt = () => {
   isDropped.value = !isDropped.value;
 };
+// const callPropsEditFunction = (info) => {
+//   props.handleEdit?(info.data)
+// }
+// const callPropsDeleteFunction = (info) => {
+//   props.handleDelete?(info.data)
+// }
+// const callPropsViewFunction = (info) => {
+//   props.handleView?(info.data)
+// }
+// const callPropsDisableFunction = (info) => {
+//   //props.handleDisable(info.data);
+//   if (info.data.disabled) {
+//     info.data.disabled = false
+//   } else {
+//     info.data.disabled = true
+//   }
+// }
+// const callPropsImpersonateFunction = (info) => {
+//   props.handleImpersonate?(info.data)
+// }
 </script>
 
 <style>
@@ -64,6 +140,8 @@ const dropIt = () => {
   transform-origin: top;
   transition: transform 0.4s ease-in-out;
   overflow: hidden;
+  cursor: pointer;
+
 }
 .list li {
   padding: 10px;
